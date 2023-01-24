041170069e301364b92eed51ff8a1fe597aa5901
ubiratanlsc <ubiratanlsc@gmail.com>
2023-01-24 18:12:07 -0300

    mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default function App() {
+
+  return (
+    <View style={styles.container}>
+      <Text> testes</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
